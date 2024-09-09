8bade0c0796de29e46ef9ce524708713746dd74d
Abdelzim Hassan <abdelazeemhassan492@gmail.com>
2024-09-09 03:23:10 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://localhost:4940/api";
+// export const API_BASE_URL = "http://localhost:4940/api";
+export const API_BASE_URL = "https://hiyam-store-dashboard.vercel.app/api";
